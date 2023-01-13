f85b7b01feedaeba96d2e5218a9381c83dd44a99
Ahmed <>
2023-01-13 21:30:05 +0300

    adding the adding as prop

--- src/features/restaurants/components/RestaurantInfoCard.js
+++ src/features/restaurants/components/RestaurantInfoCard.js
@@ -11,10 +11,20 @@ const RestaurantCardCover = styled(Card.Cover)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
+
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
 const Title = styled(Text)`
   font-family: ${(props) => props.theme.fonts.body}
-  padding: ${(props) => props.theme.space[3]};
+  font-size: ${(props) => props.theme.fontSizes.body}
   color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -33,7 +43,10 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
